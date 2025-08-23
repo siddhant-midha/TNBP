8220baf5e5ff7ff4f854ac29084521a559f78d57
siddhant-midha <siddhantm@iitb.ac.in>
2025-08-23 09:37:30 -0400

    fixed clusters for rPEPS

--- rPEPS/naiveloops.jl
+++ rPEPS/naiveloops.jl
@@ -12,7 +12,7 @@ maxiter = 1000
 N = 5
 T = N
 w = 4 
-η = 0.1
+η = 0.4
 nsamples = 1
 
 cluster_data = load_latest_cluster_file(N,w)
@@ -21,13 +21,13 @@ all_loops = [loop_object.edges for loop_object in loop_objects]
 
 
 tensors, peps = peps_controllable(N, T; η=η, ti=ti, orthog=orthog)
-exact_FE = log(real(contract_peps_no_phys(peps; cutoff=1E-12, maxdim=2^N)))
+exact_FE_density = log(real(contract_peps_no_phys(peps; cutoff=1E-12, maxdim=2^N))) / (N*T)
 
 adj_mat, edges, links = BP.get_adj_mat(tensors)
 messages = BP.get_messages(tensors, edges, links) 
 messages = BP.message_passing(tensors, messages, edges, adj_mat; α=annealing, max_iters=maxiter, diagnose=false, normalise=true)
 Z_list = BP.get_fixed_point_list(tensors, messages, adj_mat)
-bp_FE = sum(log.(real.(Z_list)))
+bp_FE_density = sum(log.(real.(Z_list))) / (N*T)
 tensors = BP.normalize_tensors(tensors, Z_list)
 
 global loopcorrx = 1.
@@ -39,6 +39,7 @@ for loop in all_loops
     end
 end
 
+correction_FE_density = log(loopcorrx) / (N*T)
 
-println("BP error: ", abs(bp_FE - exact_FE))
-println("BP + loop correction error: ", abs(bp_FE + log(loopcorrx) - exact_FE))
+println("BP error: ", abs(bp_FE_density - exact_FE_density))
+println("BP + loop correction error: ", abs(bp_FE_density + correction_FE_density - exact_FE_density))
